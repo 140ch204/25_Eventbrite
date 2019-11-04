74aec59a3c08b7c11804c25f5d4ea68b6eebbc36
140ch204 <140ch204_github@gadz.org>
2019-11-04 12:27:39 +0100

    init

---
+++ app/controllers/event_controller.rb
@@ -0,0 +1,27 @@
+class EventController < ApplicationController
+  #  include GossipHelper
+  #  before_action :authenticate_user, except: [:index, :show]
+  #  before_action :right_user, only: [:edit, :update, :destroy]
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+end
