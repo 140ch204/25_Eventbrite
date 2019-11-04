d5b7250c22f5e3b904227edf1001187d1f2a579e
140ch204 <140ch204_github@gadz.org>
2019-11-04 15:14:00 +0100

    V1

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,14 +4,14 @@ class Event < ApplicationRecord
   belongs_to :user
   
 
+  validate :divisible_by_five, :is_in_future?
+
   validates :start_date, 
     presence: {message: "Merci de saisir une date !"} #,
     #if is_in_the_futur?
     #il est impossible de créer ou modifier un événement dans le passé.
   validates :duration, 
-    presence: {message: "Merci de saisir une durée !"}#,
-    #numericality: { only_integer: true }
-    #le nombre de minutes doit être un multiple de 5, et est strictement positif
+    presence: {message: "Merci de saisir une durée !"}
   validates :title, 
     presence: {message: "Merci de saisir un titre !"},
     length: {minimum: 5, maximum: 140, message: "Le titre doit être entre 5 et 140 caractères"}
@@ -20,8 +20,21 @@ class Event < ApplicationRecord
     length: {minimum: 20, maximum: 1000, message: "La description doit être entre 20 et 1000 caractères"}
   validates :price,
     presence: {message: "Merci de saisir un prix !"},
-    numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000 }
-   # cet integer doit être compris entre 1 et 1000
+    numericality: { in: 1..1000 }
   validates :location, 
     presence: {message: "Merci de saisir une ville !"}
-end
+
+
+  def divisible_by_five
+    if (self.duration % 5) != 0
+      self.errors[:base] << "La durée doit être un multiple de 5"
+    end
+  end
+
+  def is_in_future?
+    self.start_date  >=  Date.today
+  end
+
+  end
+
+
